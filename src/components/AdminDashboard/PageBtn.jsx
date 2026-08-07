f78db026457c239a6a1725ac76ff2a4bd16bc85a
banti <banti@indihood.com>
2026-08-07 23:11:21 +0530

    ui improve

--- src/components/AdminDashboard/PageBtn.jsx
+++ src/components/AdminDashboard/PageBtn.jsx
@@ -3,7 +3,7 @@ export default function PageBtn({ children, active, onClick, disabled }) {
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`h-12 w-12 rounded-xl border border-zinc-700 transition
+      className={`h-9 w-9 rounded-xl border border-zinc-700 text-sm transition
         ${active ? "bg-zinc-800" : ""}
         ${disabled ? "cursor-not-allowed opacity-40" : "hover:bg-zinc-800"}`}
     >
